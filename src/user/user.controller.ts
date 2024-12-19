266403c3279cbd3b34d12d2978b0d60bb7e92825
NguyenCongChi <chinguyen@behdm.com>
2024-12-19 16:53:40 +0700

    update api get user/me - auth when get

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+    Body,
+    Controller,
+    Get,
+    Patch,
+    UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    constructore() { }
-
-    @UseGuards(AuthGuard('jwt'))
+    constructor() { }
     @Get('me')
-    async getMe(@Req() req: Request) {
-        return 'yourself info';
+    getMe(@GetUser() user: User) {
+        return user;
+    }
+
+    @Patch()
+    editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+
     }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,4 +4,4 @@ import { UserController } from './user.controller';
 @Module({
   controllers: [UserController]
 })
-export class UserModule {}
+export class UserModule { }
